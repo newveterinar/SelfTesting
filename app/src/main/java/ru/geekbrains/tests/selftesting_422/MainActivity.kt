d3607afc17dbfb79b813657918d5a8fb36eb1bd8
AVKhakhalin <78661461+AVKhakhalin@users.noreply.github.com>
2022-04-16 15:08:01 +0300

    Создание первого релиза приложения (#3)
    * Первый коммит на ветке "develop".
    Создание первой кнопки.
    
    * avkhakhalin (#2)
    
    * Создан пустой метод onBackPress.
    
    * Указано место, куда нужно написать алгоритм.
    
    * Merge pull request #1
    
    * Создано тело функции onBackPressed().
    
    * Указано место, куда нужно написать алгоритм.

--- app/src/main/java/ru/geekbrains/tests/selftesting_422/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/selftesting_422/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.geekbrains.tests.selftesting_422
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    /** Задание переменных */ //region
+    val button: Button = findViewById<Button>(R.id.button)
+    //endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello team!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this, "Close team!", Toast.LENGTH_SHORT).show()
     }
 }
