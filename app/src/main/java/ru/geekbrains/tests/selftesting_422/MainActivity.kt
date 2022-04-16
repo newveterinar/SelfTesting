41ae077e2b89b1e7db5016d5ac2cabb099326077
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-04-16 13:58:26 +0300

    Первый коммит на ветке "develop". Создание первой кнопки.

--- app/src/main/java/ru/geekbrains/tests/selftesting_422/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/selftesting_422/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.geekbrains.tests.selftesting_422
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    /** Задание переменных */ //region
+    val button: Button = findViewById<Button>(R.id.button)
+    //endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello team!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
